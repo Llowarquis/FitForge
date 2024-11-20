490bef3fa2ee8b107344c5cca9041c8f9e1c5ae3
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 21:36:23 -0400

    Cree la clase Tarjetas

---
+++ FitForge.Data/Models/Tarjetas.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Tarjetas
+{
+    [Key]
+    public int TarjetaId { get; set; }
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int NumeroTarjeta { get; set; }
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int Cvv { get; set; }
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public DateOnly? FechaVencimiento { get; set; } = null; 
+}
