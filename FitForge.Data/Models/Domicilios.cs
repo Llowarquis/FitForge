84c0025570d61d2e5a768ddc53415c298f6919b3
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 21:35:53 -0400

    Cree la clase Domicilios

---
+++ FitForge.Data/Models/Domicilios.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Domicilios
+{
+    [Key]
+    public int DomicilioId { get; set; }
+
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+    public string Calle { get; set; } = string.Empty;
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int NumCalle { get; set; }
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int NumCasa { get; set; }
+
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string Provincia { get; set; } = string.Empty;
+
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string Municipio { get; set; } = string.Empty;
+
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string Sector { get; set; } = string.Empty;
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string CodigoPostal { get; set; } = string.Empty;
+}
