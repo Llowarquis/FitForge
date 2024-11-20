815372c7d6a0efa9e69b0bb379df0a2b36f779f0
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 22:40:58 -0400

    Se creo el extensor del contexto

---
+++ FitForge.Data/DI/DbContextRegister.cs
@@ -0,0 +1,14 @@
+﻿using FitForge.Data.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitForge.Data.DI;
+
+public static class DbContextRegister
+{
+	public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+	{
+		services.AddDbContextFactory<Contexto>(x => x.UseSqlServer("Name=SqlConStr"));
+		return services;
+	}
+}
