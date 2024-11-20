ad516dfbd6cea07c0c12c6b2469580237e3d03b3
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 21:36:15 -0400

    Cree la clase Pagos

---
+++ FitForge.Data/Models/Pagos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Pagos
+{
+	[Key]
+	public int PagoId { get; set; }
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double Monto { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public DateTime FechaPago { get; set; } = DateTime.UtcNow;
+}
