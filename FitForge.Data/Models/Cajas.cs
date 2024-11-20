101e5273c09274bf2d83d3ae47e2306683c49923
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 21:35:40 -0400

    Cree la clase Cajas

---
+++ FitForge.Data/Models/Cajas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitForge.Data.Models;
+
+public class Cajas
+{
+	[Key] 
+	public int Id { get; set; }
+
+    public double BalanceEfectivo { get; set; }
+
+    public double BalanceBanco { get; set; } 
+
+
+    [ForeignKey("Pagos")]
+    public int PagoId { get; set; }
+    public ICollection<Pagos> Pagos { get; set; } = new List<Pagos>();
+}
