f08b5af46e02069332cc5d183ac4c282945950a2
Yohualkis <Yohualkis99@gmail.com>
2024-11-19 21:36:01 -0400

    Cree la clase Empleados

---
+++ FitForge.Data/Models/Empleados.cs
@@ -0,0 +1,26 @@
+﻿using FitForge.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Empleados : Usuarios
+{
+    [Key]
+    public int EmpleadoId { get; set; }
+
+    public int Pin { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int Nombres { get; set; }
+
+
+    [RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string Cargo { get; set; } = string.Empty;
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double Sueldo { get; set; }
+}
