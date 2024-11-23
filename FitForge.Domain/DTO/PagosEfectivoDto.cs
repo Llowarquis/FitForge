71ddc77e64b3a6b0ed073a3711d1431dd7073d62
Yohualkis <Yohualkis99@gmail.com>
2024-11-23 17:50:53 -0400

    Se creo el Dto

---
+++ FitForge.Domain/DTO/PagosEfectivoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FitForge.Domain.DTO;
+
+public class PagosEfectivoDto
+{
+    public int PagoEfectivoId { get; set; }
+	public double Monto { get; set; }
+	public DateTime FechaPago { get; set; } = DateTime.UtcNow;
+}
---
+++ FitForge.Domain/DTO/PagosTarjetaDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitForge.Domain.DTO;
+
+public class PagosTarjetaDto
+{
+	public int PagoEfectivoId { get; set; }
+	public double Monto { get; set; }
+	public DateTime FechaPago { get; set; } = DateTime.UtcNow;
+
+    [ForeignKey("TarjetasDto")]
+    public int TarjetaId { get; set; }
+    public TarjetasDto Tarjeta { get; set; }
+}
