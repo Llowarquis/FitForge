19fc3cc3c88ae90798534ee349492ac6ed7543f3
Yohualkis <Yohualkis99@gmail.com>
2024-11-25 04:22:18 -0400

    Los Dto no deben tener Foreign keys, asi que se eliminaron

--- FitForge.Domain/DTO/ClientesDto.cs
+++ FitForge.Domain/DTO/ClientesDto.cs
@@ -8,14 +8,8 @@ public class ClientesDto
     public int ClienteId { get; set; }
     public string Nombres { get; set; } = string.Empty;
     public int Pin { get; set; }
-
-    [ForeignKey("TarjetasDto")]
-    public int? TarjetaId { get; set; }
     public ICollection<TarjetasDto>? Tarjeta { get; set; } = [];
-
-    [ForeignKey("PagosEfectivo")]
-	public int PagoEfectivoId { get; set; }
-	public ICollection<PagosEfectivoDto> PagoEfectivo { get; set; } = new List<PagosEfectivoDto>();
+	public ICollection<PagosEfectivoDto> PagoEfectivo { get; set; } = [];
 	public DateOnly FechaNacimiento { get; set; }
     public EstadoMembresia Membresia { get; set; }
 }
--- FitForge.Domain/DTO/PagosTarjetaDto.cs
+++ FitForge.Domain/DTO/PagosTarjetaDto.cs
@@ -7,8 +7,5 @@ public class PagosTarjetaDto
 	public int PagoEfectivoId { get; set; }
 	public double Monto { get; set; }
 	public DateTime FechaPago { get; set; } = DateTime.UtcNow;
-
-    [ForeignKey("TarjetasDto")]
-    public int TarjetaId { get; set; }
     public TarjetasDto Tarjeta { get; set; }
 }
