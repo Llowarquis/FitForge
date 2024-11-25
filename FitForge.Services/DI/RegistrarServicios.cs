5a8f9f81eef73665a3e6e62f712568a44bcf4668
Yohualkis <Yohualkis99@gmail.com>
2024-11-25 02:46:19 -0400

    Se creo el extensor para poder inyectar los servicios y contexto al program

---
+++ FitForge.Services/DI/RegistrarServicios.cs
@@ -0,0 +1,20 @@
+﻿using FitForge.Abstractions.Interfaces;
+using FitForge.Data.DI;
+using FitForge.Services.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitForge.Services.DI;
+
+public static class RegistrarServicios
+{
+	public static IServiceCollection InyectarServicios(this IServiceCollection servicios)
+	{
+		servicios.RegisterDbContextFactory();
+		servicios.AddScoped<IClientesService, ClientesService>();
+		servicios.AddScoped<IPagosService, PagosService>();
+		servicios.AddScoped<IEmpleadosService, EmpleadosService>();
+		servicios.AddScoped<IUsuariosService, UsuariosService>();
+		servicios.AddScoped<ITarjetasService, TarjetasService>();
+		return servicios;
+	}
+}
