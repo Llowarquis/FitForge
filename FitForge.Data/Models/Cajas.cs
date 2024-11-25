ecdbc3bf3b6cd89becb086d7fb0a318fd135b12a
Yohualkis <Yohualkis99@gmail.com>
2024-11-25 04:16:26 -0400

    Se corrigieron errores con las foreign keys a las clases pagos

--- FitForge.Data/Models/Cajas.cs
+++ FitForge.Data/Models/Cajas.cs
@@ -6,19 +6,13 @@ namespace FitForge.Data.Models;
 public class Cajas
 {
 	[Key] 
-	public int Id { get; set; }
+	public int CajaId { get; set; }
 
     public double BalanceEfectivo { get; set; }
 
     public double BalanceBanco { get; set; } 
 
-
-    [ForeignKey("PagosEfectivo")]
-    public int PagoEfectivoId { get; set; }
     public ICollection<PagosEfectivo> PagosEfectivo { get; set; } = new List<PagosEfectivo>();
 
-
-	[ForeignKey("PagosTarjeta")]
-	public int PagoTarjetaId { get; set; }
 	public ICollection<PagosTarjeta> PagoTarjeta { get; set; } = new List<PagosTarjeta>();
 }
