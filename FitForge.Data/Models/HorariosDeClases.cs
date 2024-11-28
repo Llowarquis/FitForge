bbdde6ede487fc5311ae0778c6a72bcadee64022
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 19:18:15 -0400

    Se agrego el modelo HorariosDeClases

---
+++ FitForge.Data/Models/HorariosDeClases.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class HorariosDeClases
+{
+	[Key]
+	public int HorarioDeClaseId { get; set; }
+
+    public int ClaseId { get; set; }
+    public Clases Clase { get; set; }
+
+	public int DiaHorarioId { get; set; }
+	public DiasHorarios DiaHorario { get; set; }
+}
