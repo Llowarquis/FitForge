08a26de3f9546e2a5dc0bcdf084c78308ff34d66
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 19:17:00 -0400

    Se agrego el modelo Dias

---
+++ FitForge.Data/Models/Dias.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Dias
+{
+	[Key]
+	public int DiaId { get; set; }
+    public string Dia { get; set; }
+}
