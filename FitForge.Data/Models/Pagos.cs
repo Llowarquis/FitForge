2e647a8b980edd08b97e2fd12132a7851f07c1fb
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 19:18:32 -0400

    Se agrego el modelo Pagos

---
+++ FitForge.Data/Models/Pagos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitForge.Data.Models;
+
+public class Pagos
+{
+	[Key]
+	public int PagoId { get; set; }
+
+
+	[ForeignKey("Tarjetas")]
+	public int TarjetaId { get; set; }
+	public Tarjetas Tarjeta { get; set; }
+
+
+	[ForeignKey("FormasPago")]
+    public int FormasPagoId { get; set; }
+    public FormasPago FormaPago { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+    public double Monto { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public DateTime FechaPago { get; set; } = DateTime.Now;
+}
