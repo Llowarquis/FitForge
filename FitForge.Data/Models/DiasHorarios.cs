6e6457783c443f2ba1bd0af1fe10e897149dbc26
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 19:17:08 -0400

    Se agrego el modelo DiasHorarios

---
+++ FitForge.Data/Models/DiasHorarios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitForge.Data.Models;
+public class DiasHorarios
+{
+	[Key]
+	public int DiaHorarioId { get; set; }
+
+    public int DiaId { get; set; }
+    public Dias Dia { get; set; }
+
+	public int HorarioId { get; set; }
+	public Horarios Horario { get; set; }
+}
