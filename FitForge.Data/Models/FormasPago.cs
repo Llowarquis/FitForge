8ed41594c2acc887e2329a54bd7f9137831eb487
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 19:17:44 -0400

    Se agrego el modelo FormasPago

---
+++ FitForge.Data/Models/FormasPago.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class FormasPago
+{
+	[Key]
+	public int FormasPagoId { get; set; }
+    public string Descripcion{ get; set; }
+}
