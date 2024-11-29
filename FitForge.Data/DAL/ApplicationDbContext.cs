b9410224e0fecd542983d0e640a95bd7d3602df2
Yohualkis <Yohualkis99@gmail.com>
2024-11-28 23:04:30 -0400

    Se creraon los DbSet para crear las tablas en la DB

--- FitForge.Data/DAL/ApplicationDbContext.cs
+++ FitForge.Data/DAL/ApplicationDbContext.cs
@@ -1,3 +1,5 @@
+using FitForge.Data.Models;
+using FitForge.Data.Modelsp;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,4 +7,19 @@ namespace FitForge.Data.DAL;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Clases> Clases { get; set; }
+	public DbSet<Clientes> Clientes { get; set; }
+	public DbSet<Dias> Dias { get; set; }
+	public DbSet<DiasHorarios> DiasHorarios { get; set; }
+	public DbSet<Domicilios> Domicilios { get; set; }
+	public DbSet<Entrenadores> Entrenadores { get; set; }
+	public DbSet<EstadosMembresia> EstadosMembresia { get; set; }
+	public DbSet<FormasPago> FormasPago { get; set; }
+	public DbSet<Horarios> Horarios { get; set; }
+	public DbSet<HorariosDeClases> HorariosDeClases { get; set; }
+	public DbSet<Inscripciones> Inscripciones { get; set; }
+	public DbSet<Membresias> Membresias { get; set; }
+	public DbSet<Pagos> Pagos { get; set; }
+	public DbSet<Tarjetas> Tarjetas { get; set; }
+	public DbSet<Telefonos> Telefonos { get; set; }
 }
