6a4574b3526d503541ddac84167813a4f0b416a0
frailyng <frailindes@gmail.com>
2024-11-30 09:16:20 -0400

    Imembresias Service Modificado para obtener las membresias

--- FitForge.Abstractions/Interfaces/IMembresiasService.cs
+++ FitForge.Abstractions/Interfaces/IMembresiasService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FitForge.Domain.Dto;
 
 namespace FitForge.Abstractions.Interfaces
 {
-    internal interface IMembresiasService
+    public interface IMembresiasService
     {
+        Task<List<MembresiasDto>> GetMembresiasAsync();
     }
 }
