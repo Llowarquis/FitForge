8f189070728a9149abe385136e5876eaf528df84
Yohualkis <Yohualkis99@gmail.com>
2024-11-30 11:56:29 -0400

    Se agrego la clase articulos para seguir con los detalles

---
+++ FitForge.Data/Models/Articulos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitForge.Data.Models;
+
+public class Articulos
+{
+	[Key]
+	public int ArticuloId { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public int Existencia { get; set; }
+
+	public double Precio { get; set; }
+}
