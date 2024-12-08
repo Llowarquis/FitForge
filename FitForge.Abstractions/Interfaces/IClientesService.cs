653762f15e0fc68e5ce7e19447ac9a0fcc03b654
frailyng <frailindes@gmail.com>
2024-12-07 20:38:43 -0400

    Interfaz IClientesService Modificado

--- FitForge.Abstractions/Interfaces/IClientesService.cs
+++ FitForge.Abstractions/Interfaces/IClientesService.cs
@@ -5,8 +5,11 @@ namespace FitForge.Abstractions.Interfaces;
 
 public interface IClientesService
 {
-	public Task<bool> Guardar(ClientesDto clienteDto);
-	public Task<bool> Eliminar(int id);
-	public Task<ClientesDto> Buscar(int id);
-	public Task<List<ClientesDto>> Listar(Expression<Func<ClientesDto, bool>> criterio);
+    public Task<bool> Guardar(ClientesDto clienteDto);
+    public Task<bool> Eliminar(int id);
+    public Task<ClientesDto> Buscar(int id);
+    public Task<List<ClientesDto>> Listar(Expression<Func<ClientesDto, bool>> criterio);
+    Task<List<ClientesDto>> ObtenerClientes();
+
+    Task<ClientesDto?> ObtenerClientePorId(int clienteId);
 }
