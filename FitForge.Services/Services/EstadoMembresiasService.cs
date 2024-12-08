260d599180df8a0c3b4aa1ff764017215d66f862
frailyng <frailindes@gmail.com>
2024-12-07 21:02:16 -0400

    EstadoMembresiasService Creado

---
+++ FitForge.Services/Services/EstadoMembresiasService.cs
@@ -0,0 +1,28 @@
+﻿using FitForge.Abstractions.Interfaces;
+using FitForge.Data.DAL;
+using FitForge.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitForge.Services.Services
+{
+    public class EstadosMembresiasService(IDbContextFactory<ApplicationDbContext> DbFactory) : IEstadoMembresiasService
+    {
+        public async Task<List<EstadosMembresiaDto>> ObtenerEstadoMembresias()
+        {
+            await using var _contexto = await DbFactory.CreateDbContextAsync();
+            return await _contexto.EstadosMembresia
+                .Select(c => new EstadosMembresiaDto()
+                {
+                    EstadoMembresiaId = c.EstadoMembresiaId,
+                    Descripcion = c.Descripcion,
+                })
+                .AsNoTracking().ToListAsync();
+        }
+
+    }
+}
