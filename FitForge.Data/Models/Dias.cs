80acc7965681fb567b7a9cf9fcb261b8217a9f48
frailyng <frailindes@gmail.com>
2024-12-07 23:10:12 -0400

    Modelo Dia Renombrado a Nombre para mas Claridad

--- FitForge.Data/Models/Dias.cs
+++ FitForge.Data/Models/Dias.cs
@@ -6,7 +6,7 @@ public class Dias
 {
 	[Key]
 	public int DiaId { get; set; }
-    public string Dia { get; set; }
+    public string Nombre { get; set; }
 
     public ICollection<DiasHorarios> DiasHorarios { get; set; } = new List<DiasHorarios>();
 }
