c804a2bd3b629961f846fa937db08a76c4fdccfd
frailyng <frailindes@gmail.com>
2024-12-07 20:32:22 -0400

    Modelo Tarjetas Modificado el dominio NumeroTarjeta pasa de int a string

--- FitForge.Domain/DTO/TarjetasDto.cs
+++ FitForge.Domain/DTO/TarjetasDto.cs
@@ -4,7 +4,7 @@ public class TarjetasDto
 {
     public int TarjetaId { get; set; }
 	public ClientesDto Cliente { get; set; }
-	public int NumeroTarjeta { get; set; }
+	public string NumeroTarjeta { get; set; }
 	public int Cvv { get; set; }
 	public DateOnly FechaVencimiento { get; set; }
     public ICollection<PagosDto> Pagos { get; set; }
