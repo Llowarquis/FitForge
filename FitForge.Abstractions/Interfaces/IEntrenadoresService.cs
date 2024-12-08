8296e88c74b0c1e23268b8a94126f894690eea63
Yohualkis <Yohualkis99@gmail.com>
2024-12-07 20:39:40 -0400

    Se añadieron los metodos a la interfaz

--- FitForge.Abstractions/Interfaces/IEntrenadoresService.cs
+++ FitForge.Abstractions/Interfaces/IEntrenadoresService.cs
@@ -1,5 +1,12 @@
-﻿namespace FitForge.Abstractions.Interfaces;
+﻿using FitForge.Domain.DTO;
+using System.Linq.Expressions;
+
+namespace FitForge.Abstractions.Interfaces;
 
 public interface IEntrenadoresService
 {
+	public Task<bool> Guardar(EntrenadoresDto entrenadorDto);
+	public Task<bool> Eliminar(int id);
+	public Task<EntrenadoresDto> Buscar(int id);
+	public Task<List<EntrenadoresDto>> Listar(Expression<Func<EntrenadoresDto, bool>> criterio);
 }
