9cc7c5ca0ec11024169d5d17ebd4b22ac9546620
frailyng <frailindes@gmail.com>
2024-12-07 22:57:28 -0400

    Modelo Horarios Modificado

--- FitForge.Data/Models/Horarios.cs
+++ FitForge.Data/Models/Horarios.cs
@@ -8,4 +8,6 @@ public class Horarios
 	public int HorarioId { get; set; }
     public TimeOnly HoraInicio { get; set; }
     public TimeOnly HoraFin { get; set; }
+
+    public ICollection<DiasHorarios> DiasHorarios { get; set; } = new List<DiasHorarios>();
 }
