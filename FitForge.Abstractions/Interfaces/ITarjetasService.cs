31a1ecb3c1364c6ac69a16ce68bad5d2dfc6d3c7
frailyng <frailindes@gmail.com>
2024-12-08 02:38:01 -0400

    Interfaz ITarjertasService Creado

--- FitForge.Abstractions/Interfaces/ITarjetasService.cs
+++ FitForge.Abstractions/Interfaces/ITarjetasService.cs
@@ -1,5 +1,16 @@
-﻿namespace FitForge.Abstractions.Interfaces;
+﻿using FitForge.Data.Models;
+using FitForge.Domain.DTO;
+using System.Linq.Expressions;
+
+namespace FitForge.Abstractions.Interfaces;
 
 public interface ITarjetasService
 {
+    Task<bool> Guardar(TarjetasDto tarjetasDto);
+    Task<bool> Insertar(TarjetasDto tarjetasDto);
+    Task<bool> Modificar(TarjetasDto tarjetasDto);
+    Task<bool> Existe(int id);
+    Task<bool> Eliminar(int id);
+    Task<TarjetasDto?> Buscar(int id);
+    Task<List<TarjetasDto>> Listar(Expression<Func<Tarjetas, bool>> criterio);
 }
