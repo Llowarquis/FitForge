18b257d051fadffb8abed83f6640feafe2ff5747
frailyng <frailindes@gmail.com>
2024-12-07 20:26:13 -0400

    Modelo de Inscripciones modificado y Inscripcionesdetalle creado

---
+++ FitForge.Data/Models/InscripcionesDetalle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitForge.Data.Models;
+
+public class InscripcionesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int InscripcionId { get; set; }
+
+    public int ItinerarioId { get; set; }
+
+    public int ClaseId { get; set; }
+
+    public int DiaHorarioId { get; set; }
+
+    public int EntrenadorId { get; set; }
+
+    public double Precio { get; set; }
+}
--- FitForge.Domain/DTO/InscripcionesDto.cs
+++ FitForge.Domain/DTO/InscripcionesDto.cs
@@ -4,11 +4,17 @@ namespace FitForge.Domain.DTO;
 
 public class InscripcionesDto
 {
-	public int InscripcionId { get; set; }
-	public ClientesDto Cliente { get; set; }
-	public MembresiasDto Membresia { get; set; }
-	public ItinerariosDto Itinerario { get; set; }
-	public DateTime FechaInscripcion { get; set; } = DateTime.Now;
-	public double Precio { get; set; }
-	public EntrenadoresDto Entrenador { get; set; }
+    public int InscripcionId { get; set; }
+
+    public int ClienteId { get; set; }
+    public ClientesDto Cliente { get; set; }
+
+    public int MembresiaId { get; set; }
+    public MembresiasDto Membresia { get; set; }
+
+    public DateTime FechaInscripcion { get; set; } = DateTime.Now;
+
+    public double Precio { get; set; }
+
+    public virtual ICollection<InscripcionesDetalleDto> InscripcionDetalle { get; set; } = new List<InscripcionesDetalleDto>();
 }
