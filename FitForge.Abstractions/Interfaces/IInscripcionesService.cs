7e7f292753be7b011a58aa8279fe44193ec08e36
frailyng <frailindes@gmail.com>
2024-12-08 02:33:55 -0400

    Interfaz IInscripcionesService Creado

---
+++ FitForge.Abstractions/Interfaces/IInscripcionesService.cs
@@ -0,0 +1,30 @@
+﻿using FitForge.Domain.DTO;
+using FitForge.Data.Modelsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitForge.Abstractions.Interfaces;
+
+public interface IInscripcionesService
+{
+    Task<bool> Existe(int inscripcionId);
+    Task<bool> Insertar(InscripcionesDto inscripcionesDto);
+
+    Task AfectarInscripcion(InscripcionesDetalleDto[] detalle);
+
+    Task<bool> Modificar(InscripcionesDto inscripcionesDto);
+
+    Task<bool> Guardar(InscripcionesDto inscripcionesDto);
+
+    Task<InscripcionesDto> Buscar(int id);
+
+    Task<bool> Eliminar(int id);
+
+    Task<List<InscripcionesDto>> Listar(Expression<Func<Inscripciones, bool>> criterio);
+
+    Task<IEnumerable<InscripcionesDetalleDto>> ObtenerDetalles(int inscripcionId);
+}
