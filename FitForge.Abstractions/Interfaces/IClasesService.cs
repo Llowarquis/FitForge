8fdd6ffc930db733b897469d702ab9590957eb1f
Yohualkis <Yohualkis99@gmail.com>
2024-12-07 20:39:29 -0400

    Se añadio la interfaz de clases

---
+++ FitForge.Abstractions/Interfaces/IClasesService.cs
@@ -0,0 +1,12 @@
+﻿using FitForge.Domain.DTO;
+using System.Linq.Expressions;
+
+namespace FitForge.Abstractions.Interfaces;
+
+public interface IClasesService
+{
+	public Task<bool> Guardar(ClasesDto clasesDto);
+	public Task<bool> Eliminar(int id);
+	public Task<ClasesDto> Buscar(int id);
+	public Task<List<ClasesDto>> Listar(Expression<Func<ClasesDto, bool>> criterio);
+}
