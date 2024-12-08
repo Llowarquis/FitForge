4c40e151e325c90944eae1c92296b0a5080b9b38
Yohualkis <Yohualkis99@gmail.com>
2024-12-07 20:40:19 -0400

    Se añadio el campo Precio

--- FitForge.Domain/DTO/ClasesDto.cs
+++ FitForge.Domain/DTO/ClasesDto.cs
@@ -3,8 +3,10 @@
 public class ClasesDto
 {
 	public int ClaseId { get; set; }
-    public string NombreEntrenador { get; set; }
     public string Descripcion { get; set; }
-	public int Cupos { get; set; }
-    public DateTime FechaVencimiento { get; set; }
+    public double Precio { get; set; }
+    public int Cupos { get; set; }
+
+	// Esta fecha sera igual a la fecha de inscripcion mas 1 mes
+	public DateTime FechaVencimiento { get; set; }
 }
