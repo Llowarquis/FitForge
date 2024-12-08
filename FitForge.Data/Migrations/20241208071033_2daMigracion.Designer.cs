ef2f1e5f1d0dde52de4529e1a426058fa32acd9e
Yohualkis <Yohualkis99@gmail.com>
2024-12-08 04:09:13 -0400

    Se migraron los datos

--- FitForge.Data/Migrations/20241208071033_2daMigracion.Designer.cs
+++ FitForge.Data/Migrations/20241208071033_2daMigracion.Designer.cs
@@ -4,6 +4,7 @@ using FitForge.Data.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitForge.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241208071033_2daMigracion")]
+    partial class _2daMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
