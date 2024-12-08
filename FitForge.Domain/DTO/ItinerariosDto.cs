d95cc9c18de0eb7be5a552a0bc0c98b68cdbb0e6
Yohualkis <Yohualkis99@gmail.com>
2024-12-07 20:42:01 -0400

    Se cambio el nombre a una propiedad

--- FitForge.Domain/DTO/ItinerariosDto.cs
+++ FitForge.Domain/DTO/ItinerariosDto.cs
@@ -2,8 +2,10 @@
 
 public class ItinerariosDto
 {
+	// A esta clase es la que se le hara el registro de las clases disponibles
+
 	public int ItinerarioId { get; set; }
-    public string NombreClase { get; set; }
+    public string DescripcionClase { get; set; }
 	public List<string> Dia { get; set; } = new List<string>();
     public TimeOnly HoraInicio { get; set; }
 	public TimeOnly HoraFin{ get; set; }
