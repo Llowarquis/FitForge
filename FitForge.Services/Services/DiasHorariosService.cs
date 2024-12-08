930940dbcaf99f5bf78d8b3fb9af12dddd9ac1d1
frailyng <frailindes@gmail.com>
2024-12-08 02:44:19 -0400

    DiasHorariosService Creado

---
+++ FitForge.Services/Services/DiasHorariosService.cs
@@ -0,0 +1,29 @@
+﻿using FitForge.Abstractions.Interfaces;
+using FitForge.Data.DAL;
+using FitForge.Domain.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitForge.Services.Services;
+
+public class DiasHorariosService(IDbContextFactory<ApplicationDbContext> DbFactory) : IDiasHorariosService
+{
+    public async Task<List<DiasHorariosDto>> ObtenerDiasHorariosAsync()
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+        return await _contexto.DiasHorarios
+            .Include(dh => dh.Dia)
+            .Include(dh => dh.Horario)
+            .Select(dh => new DiasHorariosDto
+            {
+                DiaHorarioId = dh.DiaHorarioId,
+                DiaHorarioDescripcion = $"{dh.Dia.Nombre} - {dh.Horario.HoraInicio:HH:mm} a {dh.Horario.HoraFin:HH:mm}"
+            })
+            .ToListAsync();
+    }
+
+}
