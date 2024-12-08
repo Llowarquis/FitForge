cb7491a3e57578e3e20d97c344cc9caf05d989a4
Yohualkis <Yohualkis99@gmail.com>
2024-12-07 20:40:50 -0400

    Se reemplazaron los tipos de datos ya que el dto debe ser ligero

--- FitForge.Domain/DTO/DiasHorariosDto.cs
+++ FitForge.Domain/DTO/DiasHorariosDto.cs
@@ -4,8 +4,9 @@ public class DiasHorariosDto
 	public int DiaHorarioId { get; set; }
 
     public int DiaId { get; set; }
-    public DiasDto Dia { get; set; }
+    public string NombreDia { get; set; }
 
 	public int HorarioId { get; set; }
-	public HorariosDto Horario { get; set; }
+	public TimeOnly HoraInicio { get; set; }
+	public TimeOnly HoraFin { get; set; }
 }
--- FitForge.Domain/DTO/InscripcionesDto.cs
+++ FitForge.Domain/DTO/InscripcionesDto.cs
@@ -1,14 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace FitForge.Domain.DTO;
+﻿namespace FitForge.Domain.DTO;
 
 public class InscripcionesDto
 {
 	public int InscripcionId { get; set; }
-	public ClientesDto Cliente { get; set; }
-	public MembresiasDto Membresia { get; set; }
-	public ItinerariosDto Itinerario { get; set; }
+
+	// Cliente
+	public int ClienteId { get; set; }
+    public string NombreCliente { get; set; }
+
+	// Entrenador
+	public int? EntrenadorId { get; set; }
+    public string? NombreEntrenador { get; set; }
+
+	// Itinerario
+	public int? ItinerarioId { get; set; }
+    public string? NombreClase { get; set; }
+    public string? DiaClase { get; set; }
+	public TimeOnly? HoraInicioClase { get; set; }
+	public TimeOnly? HoraFinClase { get; set; }
+    public double PrecioClase { get; set; }
+
+    // Membresia
+    public string DescripcionMembresia { get; set; }
+
 	public DateTime FechaInscripcion { get; set; } = DateTime.Now;
-	public double Precio { get; set; }
-	public EntrenadoresDto Entrenador { get; set; }
+
+	// Este precio la suma del precio de la inscripcion de membresia, clases o entrenadores
+	public double Precio { get; set; } 
 }
